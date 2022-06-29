c7f227e20275ae2e6f4a76db95e79a40c4a65b85
RobsonMT <rbsndev3@gmail.com>
2022-06-29 13:03:00 -0300

    feat: stepOne page

--- src/pages/StepOne.tsx
+++ src/pages/StepOne.tsx
@@ -1,8 +1,18 @@
 import { MotionContainer } from "../components/MotionContainer";
-import { Grid } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Flex,
+  Grid,
+  Select,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
 // import { useEffect, useState } from "react";
 import { Steps } from "../components/Steps";
 import { Header } from "../components/Header";
+import { ChevronRightIcon } from "@chakra-ui/icons";
+import { theme } from "../styles/theme";
 
 export const StepOne = () => {
   // const [state, setState] = useState("");
@@ -21,6 +31,99 @@ export const StepOne = () => {
       <Header />
       <Grid minH="100vh" justifyItems="center" alignItems="center">
         <Steps />
+        <Flex
+          flexDir="column"
+          gap="20px"
+          marginTop="20px"
+          padding={["20px 40px", "20px 40px", "20px", "20px"]}
+          w={["100%", "100%", "50%", "50%"]}
+          maxW={["100%", "100%", "500px", "500px"]}
+        >
+          <Box>
+            <Text fontSize="sm" fontWeight="bold" textAlign="center">
+              Você escolheu:
+            </Text>
+            <Select
+              placeholder="Digite aqui o email"
+              textAlign="center"
+              bg="whitesmoke"
+              h="30px"
+              borderRadius="15px"
+              color="pink"
+            >
+              <option value="option1">Option 1</option>
+              <option value="option2">Option 2</option>
+              <option value="option3">Option 3</option>
+            </Select>
+          </Box>
+          <Box>
+            <Text fontSize="sm" fontWeight="bold" textAlign="center">
+              Formato do atendimento
+            </Text>
+            <Select
+              placeholder="Digite aqui o email"
+              textAlign="center"
+              bg="whitesmoke"
+              h="30px"
+              borderRadius="15px"
+              color="pink"
+            >
+              <option value="option1">Option 1</option>
+              <option value="option2">Option 2</option>
+              <option value="option3">Option 3</option>
+            </Select>
+          </Box>
+          <Box>
+            <Text fontSize="sm" fontWeight="bold" textAlign="center">
+              Insira o e-mail utilizado na compra
+            </Text>
+            <Select
+              placeholder="Digite aqui o email"
+              textAlign="center"
+              bg="whitesmoke"
+              h="30px"
+              borderRadius="15px"
+              color="pink"
+            >
+              <option value="option1">Option 1</option>
+              <option value="option2">Option 2</option>
+              <option value="option3">Option 3</option>
+            </Select>
+          </Box>
+          <VStack>
+            <Button
+              bg="pink"
+              borderRadius="15px"
+              border="2px solid"
+              color="gray.200"
+              h="30px"
+              display="flex"
+              justifyContent="space-spaceBetween"
+              alignItems="center"
+              padding="5px 5px 5px 30px"
+            >
+              CONTINUAR
+              <ChevronRightIcon
+                h="20px"
+                w="20px"
+                color="purple.800"
+                border={`2px solid ${theme.colors.purple[800]}`}
+                borderRadius="50%"
+                marginLeft="10px"
+              />
+            </Button>
+            <Button
+              bg="transparent"
+              borderRadius="15px"
+              border="2px solid"
+              color="gray.200"
+              h="30px"
+              p="5px 30px"
+            >
+              ALTERAR
+            </Button>
+          </VStack>
+        </Flex>
       </Grid>
     </MotionContainer>
   );
